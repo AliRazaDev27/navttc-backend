04d0e5d93c8753f57627d38d1024a9b21f13c4ae
UrwaByte <urwasultan9@gmail.com>
2025-11-19 03:49:20 -0800

    added getAllUsers

---
+++ src/controllers/userController.ts
@@ -0,0 +1,10 @@
+import type { Request, Response } from "express"
+import userModel from "../models/userModel.js"
+const getAllUsers = async (req: Request, res: Response) => {
+    try {
+        const users = await userModel.find();
+        res.status(200).json({success:true,data:users});
+    } catch (error) {
+        res.status(400).json({status:false,data:[]})
+    }
+}
